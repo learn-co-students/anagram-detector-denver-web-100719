f4c4618e19a0915d8780e5b40c6c7bf7d75435f0
Nepsaco <tobie.tsuzuki@gmail.com>
2019-10-13 11:34:18 -0600

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+ attr_reader :string
+    @@all = []
+
+    def initialize(string)
+        @string = string
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def match(arg_string)
+        arg_string.select{|word| word.split('').sort ==string.split('').sort}
+        # binding.pry
+    end
+    
+    
+    Anagram.new('diaper')
+    all[0].match(%w(hello world zombies pants dipper))
+
+end
